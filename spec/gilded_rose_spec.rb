c9dabdd9d54ad3374266ca99384ef4f1c2044d2f
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-11-29 19:14:44 +0000

    Add update_quality and change sell_in

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,4 +1,5 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+# require File.join(File.dirname(__FILE__), 'gilded_rose')
+require 'gilded_rose'
 
 describe GildedRose do
 
@@ -6,8 +7,17 @@ describe GildedRose do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
     end
+
+  it 'reduces quality and sell_in each day for normal goods' do
+    items = [Item.new("foo", 11, 11)]
+    GildedRose.new(items)
+    expect{ GildedRose.new(items).update_quality }.to change{ items[0].sell_in }.by(-1)
+    # expect(items[0].update_quality).to change{ items[0].value }.by(-1)
+     expect{ GildedRose.new(items).update_quality }.to change{ items[0].quality }.by(-1)
   end
+end
+
 
 end
